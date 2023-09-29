56245a39e906260314c1dfa383c7a2b4c83a14d4
Gonzalo Fleming <gonzafg2@gmail.com>
2023-09-29 01:50:28 -0300

    fix: :rocket: Files to render from server

--- app.rb
+++ app.rb
@@ -1,85 +1,11 @@
-require 'rest-client'
-require 'json'
-require 'erb'
-require_relative 'helpers/date'
+require 'sinatra'
+require_relative 'app/index.rb'
 
-URL_BASE = 'https://www.buda.com/api/v2'
-
-# Get the IDs of the transactional markets
-def getMarkets
-  begin
-    res = RestClient.get("#{URL_BASE}/markets")
-    return JSON.parse(res)['markets'].map { |market| market['id'] }
-  rescue RestClient::ExceptionWithResponse => e
-    if e.response
-      e_message = JSON.parse(e.response)["message"]
-      puts "Error in the markets API call: #{e_message}"
-    else
-      puts "Generic error in the markets API call."
-    end
-  rescue JSON::ParserError => e
-    puts "Error processing markets API response as JSON: #{e.message}"
-  rescue => e
-    puts "An unknown error occurred in the markets API: #{e}"
-  end
+configure do
+  generateReport
+  puts "Initializing app..."
 end
 
-# Gets the transactions of a market in the last 24 hours
-def getTransactionsAndValue24h(market)
-  begin
-    res = RestClient.get("#{URL_BASE}/markets/#{market}/trades?timestamp=#{timestampDayAgo}&limit=100")
-    entries = JSON.parse(res)['trades']['entries']
-    return entries.map { |transaction| {
-      timestamp: transaction[0].to_i,
-      amount: transaction[1].to_f,
-      price:  transaction[2].to_f,
-      direction: transaction[3],
-      value: transaction[1].to_f * transaction[2].to_f
-    }}
-  rescue RestClient::ExceptionWithResponse => e
-    if e.response
-      e_message = JSON.parse(e.response)["message"]
-      puts "Error in the call to the trades API: #{e_message}"
-    else
-      puts "Generic error in the trades API call."
-    end
-  rescue JSON::ParserError => e
-    puts "Error processing trades API response as JSON: #{e.message}"
-  rescue => e
-    puts "An unknown error occurred in the trades API: #{e}"
-  end
+get '/' do
+  send_file 'app/views/index.html'
 end
-
-# Get the highest value transaction in the last 24 hours
-def getHighestTransaction24h(market)
-  begin
-    return getTransactionsAndValue24h(market).max_by { |transaction| transaction[:value] }
-  rescue => e
-    puts "Error obtaining the highest value transaction on the market #{market}: #{e}."
-  end
-end
-
-# Get the highest value transaction in the last 24 hours of all markets
-def getHighestTransactions24h
-  begin
-    return getMarkets.map { |market| [market, getHighestTransaction24h(market)] }.to_h
-  rescue => e
-    puts "Error getting the highest value transactions from all markets in the last 24 hours: #{e}."
-  end
-end
-
-# Generate the HTML file with the information of the highest value transactions in the last 24 hours
-def generateReport
-  template_html = File.read('pages/index.html.erb')
-  template = ERB.new(template_html)
-  highest_transactions = getHighestTransactions24h
-  html_content = template.result(binding)
-  begin
-    File.write('index.html', html_content)
-    puts "File 'index.html' successfully generated."
-  rescue => e
-    puts "Error generating file 'index.html': #{e}."
-  end
-end
-
-generateReport
